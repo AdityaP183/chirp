7c060e38271e2565922fc0fd57f8bae419d45a6b
AdityaP183 <adityaprasad1828@gmail.com>
2025-07-22 18:53:40 +0530

    app: added routing via tabs

---
+++ mobile/app/(tabs)/index.tsx
@@ -0,0 +1,12 @@
+import SignOutButton from "@/components/signout-btn";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+const HomeScreen = () => {
+	return (
+		<SafeAreaView className="flex-1">
+			<Text>HomeScreen</Text>
+            <SignOutButton/>
+		</SafeAreaView>
+	);
+};
+export default HomeScreen;
---
+++ mobile/app/(tabs)/messages.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+const MessagesScreen = () => {
+  return (
+    <View>
+      <Text>Messages</Text>
+    </View>
+  )
+}
+export default MessagesScreen
